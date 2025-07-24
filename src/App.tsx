85dbc343af55d25857f2e9d8b19253d7d64a1ded
pavan kumar kuchibhotla <22mh1a42e6@acoe.edu.in>
2025-07-24 23:34:10 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Intro from './components/Intro';
 import Projects from './components/Projects';
 import Blogs from './components/Blogs';
